88fb0b65df5444cf84eb3097832e3674a9e49c7f
Michael Won <michaelwon@Yuseoks-Mac.local>
2022-04-22 22:50:15 +0900

    added cardNFT contract

---
+++ contracts/CardNFT.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+import "./BusinessCard.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract CardNFT is BusinessCard, ERC1155{
+    ;
+}
